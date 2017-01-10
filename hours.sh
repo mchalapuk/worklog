11719fb316dab862c136182ff835c836398fc782
Maciej Chałapuk <m.chalapuk@quedex.net>
2017-01-10 18:42:56 +0100

    + checking DATA_DIR variable

--- hours.sh
+++ hours.sh
@@ -6,7 +6,11 @@ die() {
 }
 
 CONFIG_FILE="$HOME/.worklog"
-test -f "$CONFIG_FILE" || die "no config file found at $CONFIG_FILE"
+test -f "$CONFIG_FILE" || \
+  die "no config file found; run \`touch $CONFIG_FILE\` to correct this"
 
+. "$CONFIG_FILE"
+test "$DATA_DIR" != "" || \
+  die "DATA_DIR variable not defined; please add it to $CONFIG_FILE"
 
 
