5eedbd25ebb9b9eda24e24cfd0c568d9921c1af1
Maciej Chałapuk <m.chalapuk@quedex.net>
2017-01-10 18:47:37 +0100

    + usage

--- hours.sh
+++ hours.sh
@@ -16,3 +16,22 @@ test "$DATA_DIR" != "" || \
 test -d "$DATA_DIR" || \
   die "DATA_DIR=$DATA_DIR is not a directory; run \`mkdir -p $DATA_DIR\` to correct it"
 
+PRG=$0
+
+usage() {
+  echo "Usage: $PRG command (arguments...)"
+  echo ""
+  echo "  Commands:"
+}
+
+CMD=`shift`
+
+case "$CMD" in
+  *)
+    echo "unknown command: $CMD" >&2
+    echo "" >&2
+    usage >&2
+    exit 1
+    ;;
+esac
+
