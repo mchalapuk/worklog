bc93b51c994c96339df8e5699de72e4d03674c36
Maciej Chałapuk <m.chalapuk@quedex.net>
2017-02-21 13:41:48 +0100

    + start and stop logging lightdm scripts

---
+++ lightdm/stop_logging.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+HOURS="$HOME/bin/hours" 
+
+if ! [ -f "$HOURS" ]
+then
+  touch "$HOME/.no_hours"
+  exit 1;
+fi
+
+OUTPUT=`$HOURS log OUT 2>&1`
+test -n "$OUTPUT" && echo "`date` :: $OUTPUT" >> "$HOME/.hours.log"
+exit 0
+
